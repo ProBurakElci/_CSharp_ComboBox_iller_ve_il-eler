13647f324ba04beb1111a114c97b5933787d26c9
Burak Elçi <burakelci12@gmail.com>
2023-11-01 21:16:48 +0300

    Proje dosyası ekle.

---
+++ _2_ListBox_ComboBox/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace _2_ListBox_ComboBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbSehirler = new System.Windows.Forms.ComboBox();
+            this.cmbIlceler = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cmbSehirler
+            // 
+            this.cmbSehirler.FormattingEnabled = true;
+            this.cmbSehirler.Location = new System.Drawing.Point(216, 71);
+            this.cmbSehirler.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cmbSehirler.Name = "cmbSehirler";
+            this.cmbSehirler.Size = new System.Drawing.Size(160, 24);
+            this.cmbSehirler.TabIndex = 0;
+            this.cmbSehirler.SelectedIndexChanged += new System.EventHandler(this.cmbSehirler_SelectedIndexChanged);
+            // 
+            // cmbIlceler
+            // 
+            this.cmbIlceler.FormattingEnabled = true;
+            this.cmbIlceler.Location = new System.Drawing.Point(416, 71);
+            this.cmbIlceler.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cmbIlceler.Name = "cmbIlceler";
+            this.cmbIlceler.Size = new System.Drawing.Size(160, 24);
+            this.cmbIlceler.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(263, 28);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "ŞEHİRLER";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(463, 28);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "İLÇELER";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(801, 393);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbIlceler);
+            this.Controls.Add(this.cmbSehirler);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbSehirler;
+        private System.Windows.Forms.ComboBox cmbIlceler;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ _2_ListBox_ComboBox/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2_ListBox_ComboBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void cmbSehirler_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cmbIlceler.Items.Clear(); // Her seçimde önceki ilçeleri temizle
+
+            if (cmbSehirler.SelectedIndex == 0)
+            {
+                cmbIlceler.Items.Add("Karşıyaka");
+                cmbIlceler.Items.Add("Narlıdere");
+                cmbIlceler.Items.Add("Güzelbahçe");
+            }
+            else if (cmbSehirler.SelectedIndex == 1)
+            {
+                cmbIlceler.Items.Add("Büyükşehir");
+                cmbIlceler.Items.Add("Taksim");
+                cmbIlceler.Items.Add("Büyükçekmece");
+                cmbIlceler.Items.Add("Küçükçekmece");
+                cmbIlceler.Items.Add("Altınboynuz");
+                cmbIlceler.Items.Add("Avrupa yakası");
+            }
+            if (cmbSehirler.SelectedItem == "Ankara")
+            {
+                cmbIlceler.Items.Add("Karşıyaka");
+                cmbIlceler.Items.Add("Narlıdere");
+                cmbIlceler.Items.Add("Güzelbahçe");
+                cmbIlceler.SelectedIndex = 0;
+            }
+
+            else if (cmbSehirler.SelectedItem == "Şırnak")
+            {
+                cmbIlceler.Items.Add("Cizre");
+                cmbIlceler.Items.Add("Silopi");
+                cmbIlceler.Items.Add("İdil");
+                cmbIlceler.Items.Add("Uludere");
+                cmbIlceler.Items.Add("Beytüşebap");
+                cmbIlceler.Items.Add("Roma");
+                cmbIlceler.SelectedIndex = 0;
+
+            }
+            else if (cmbSehirler.SelectedItem == "İstanbul")
+            {
+                cmbIlceler.Items.Add("BÜyükşehir");
+                cmbIlceler.Items.Add("Taksim");
+                cmbIlceler.Items.Add("Büyükçekmece");
+                cmbIlceler.Items.Add("Küçükçekmece");
+                cmbIlceler.Items.Add("Altınboynuz");
+                cmbIlceler.Items.Add("Avrupa yakası");
+                cmbIlceler.SelectedIndex = 0;
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmbSehirler.Items.Add("Ankara");
+            cmbSehirler.Items.Add("İstanbul");
+            cmbSehirler.Items.Add("Eskişehir");
+            cmbSehirler.Items.Add("Şırnak");
+            cmbSehirler.Items.Add("Diyarbakır");
+            cmbSehirler.Items.Add("Bingöl");
+            cmbSehirler.Items.Add("Hakkari");
+            cmbSehirler.SelectedIndex = 0;
+
+
+        }
+
+        
+    }
+}
